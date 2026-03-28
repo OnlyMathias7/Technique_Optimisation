335f7761a08ea1c1d37249bae5011da9fc8025fd
OnlyMathias7 <mathias.saldanha@gmail.com>
2026-03-28 15:59:53 +0100

    Correction d'erreur

--- programme_1.m
+++ programme_1.m
@@ -6,18 +6,23 @@ f_x3 = @(x) (4*x.^3) + (4*x.^2) - 15*x - 18 ;  %[0; 2, 5]
 f_x4 = @(x) (x - 1).^3 -x;
 f_x5 = @(x) (x.^3)-x;
 f_x6 = @(x) (x - 1).^2 - 2 ;%sur [−3; 4.5]
-f_x7 = @(x) (x - 1).^4-2*x2+x-1; %sur [−3; 4.5]
+f_x7 = @(x) (x - 1).^4-2*(x.^2)+x-1; %sur [−3; 4.5]
 
 
+
+%Exercice 2
+calc_affichage(f_x,3,4, sqrt(10), 1);
 
 %Exercice 3
-calc_affichage(f_x,3,4, sqrt(10), 1);
 calc_affichage(f_x2,0,5, sqrt(2), 1);
 calc_affichage(f_x3,0,2, 0, 2);
 
 %Exercice 4
 calc_affichage(f_x4,1,2.5,0, 2);
 calc_affichage(f_x5,0,4.5, 0, 2);
-calc_affichage(f_x6,)
+calc_affichage(f_x6,-3, 4.5,0,2);
+calc_affichage(f_x7, -3, 4.5, 0,2);
+
+%Exercice 8
 
 
