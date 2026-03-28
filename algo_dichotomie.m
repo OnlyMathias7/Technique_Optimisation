e82bffadd132bdb24ebf355d02046f9589956cb3
OnlyMathias7 <mathias.saldanha@gmail.com>
2026-03-28 15:51:12 +0100

    On a retiré les disp des algos

--- algo_dichotomie.m
+++ algo_dichotomie.m
@@ -1,9 +1,9 @@
-function valeur = algo_dichotomie(f,a,b)
+function [valeur,n] = algo_dichotomie(f,a,b)
 
 %Algo de dichotomie
 k=10^(-9);
 flag = true;
-N=0;
+n=0;
 while(flag)
     c=(a+b)/2;
     if (f(a)*f(c)<=0) 
@@ -11,13 +11,10 @@ while(flag)
     else 
         a=c;
     end
-    N=N+1;
+    n=n+1;
     if(abs(b-a)<k)
-        disp('On a trouver une valeur');
         flag = false;
         valeur=(a+b)/2;
     end
 end
-disp('On a fait itération :');
-disp(N);
 end 
