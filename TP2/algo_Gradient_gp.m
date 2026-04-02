7396c19e8c1039266d8068ef63a3bcb138dd9832
OnlyMathias7 <mathias.saldanha@gmail.com>
2026-04-02 11:34:44 +0200

    Fin du TP

---
+++ TP2/algo_Gradient_gp.m
@@ -0,0 +1,6 @@
+function u0 = algo_Gradient_gp(GJ, J, A, B)
+
+
+
+
+end 
--- TP2/exercice16.m
+++ TP2/exercice16.m
@@ -1,2 +1,10 @@
 clc, clear, close all
 
+A = [a c; c d];
+B = [-2 ; 1];
+
+
+J_u = @(u) 0.5*u'*A*u-B'*u+c;
+GJ_u = @(u) A*u-B;
+
+nI
